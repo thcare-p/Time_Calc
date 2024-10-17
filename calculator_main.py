54f3dd682aeeb8f025824d37ba6b22d87e142938
Pavel thCare <cull9867@gmail.com>
2024-10-17 16:59:07 +0200

    finish

--- calculator_main.py
+++ calculator_main.py
@@ -1,1 +1,25 @@
-test = 1
+#в какой-то из функции необходимо будет делать рекурсию, так как при значениях, где увеличивается сразу несколько дней, должны меняться знаки AM/PM.
+#Хотя возможен вариант подсчёта количства изменений и в последствии примитивного обозначения времени
+#Дни надо будет закинуть в dict{}
+
+#функция для проверки присваемого значения AM/PM
+def check_half(n, n1, prefix):
+    
+    pass
+
+#функция для смены дня в последствии сочетания 
+def change_day(n, n1, prefix):
+    #как-то надо обдумать какую-никакую рекурсию
+    pass
+
+#основная функция
+def add_time(start, duration):
+    pass
+
+start_input = "3:10"
+duration_input = "3:00 PM"
+
+# start_input = input("Add start time: ")
+# duration_input = input("Write amount of hours: ")
+
+add_time(start_input, duration_input)

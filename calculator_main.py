a9b47493f5def3edc67b9bb7c574e12343670ab7
Pavel thCare <cull9867@gmail.com>
2024-10-21 17:50:45 +0200

    17:50

--- calculator_main.py
+++ calculator_main.py
@@ -1,23 +1,25 @@
 #в какой-то из функции необходимо будет делать рекурсию, так как при значениях, где увеличивается сразу несколько дней, должны меняться знаки AM/PM.
 #Хотя возможен вариант подсчёта количства изменений и в последствии примитивного обозначения времени
 #Дни надо будет закинуть в dict{}
-def time_split(start_s, prefix):
+def time_split(start_s, prefix=False):
     if not prefix:
-        _list = list(start_s)
-        start_split = _list[0].split(":")
-        for time in start_split:
-            hours, minutes = time
+        # _list = list(start_s)
+        # print(_list)
+        hours = start_s[0]
+        minutes = start_s[2] + start_s[3]
         return hours, minutes
     else:
         start_split = start_s.split()#мы делим начальную строку на лист по пробелу, а потом по :
+        print(start_split)
         start_prefix = start_split[1]
         if start_prefix == "AM":
             digit_prefix = 1
         else:
             digit_prefix = 2
-        start_s_split = start_split[0].split[":"]
-        for time in start_s_split:
-            hours, minutes = time 
+        start_s_split = start_split[0].split(":")
+        hours = start_s_split[0]
+        minutes = start_s_split[1]
+
         return hours, minutes, digit_prefix
         
             
@@ -31,11 +33,11 @@ def change_day(n, n1, prefix):
 
 #основная функция
 def add_time(start, duration):
-    start_hours, start_minutes = time_split(start, True)
+    start_hours, start_minutes, start_prefix = time_split(start, True)
     duration_hours, duration_minutes = time_split(duration)
-    return start_hours, start_minutes
+    return start_hours, start_minutes, start_prefix,  duration_hours, duration_minutes
 
-start_input = "3:10 PM"
+start_input = "3:00 PM"
 duration_input = "3:00"
 
 # start_input = input("Add start time: ")
---
+++ test.py
@@ -0,0 +1,3 @@
+test_string = "05"
+res = int(test_string) + 5
+print(res)

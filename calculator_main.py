f6203c7b7ac31f99559f554c4bf928685395f936
Pavel thCare <cull9867@gmail.com>
2024-10-22 18:22:02 +0200

    18:21

--- calculator_main.py
+++ calculator_main.py
@@ -1,76 +1,101 @@
-#в какой-то из функции необходимо будет делать рекурсию, так как при значениях, где увеличивается сразу несколько дней, должны меняться знаки AM/PM.
-#Хотя возможен вариант подсчёта количства изменений и в последствии примитивного обозначения времени
-#Дни надо будет закинуть в dict{}
 def time_split(start_s, prefix=False):
+    hours = ""
+    minutes = ""
     if not prefix:
-        hours = start_s[0]
-        minutes = start_s[2] + start_s[3]
+        for i in start_s:
+            if i != ":":
+                hours += str(i)
+            else:
+                index = start_s.index(":")
+                break
+        for i in range(index + 1, len((start_s))):
+            minutes += start_s[i]
         return hours, minutes
     else:
-        start_split = start_s.split()#мы делим начальную строку на лист по пробелу, а потом по :
+        start_split = start_s.split()#мы делим начальную строку на лист по пробелу
+        for i in start_split[0]:
+            if i != ":":
+                hours += str(i)
+            else:
+                index = start_split[0].index(":")
+                break
+        for i in range(index + 1, len((start_split[0]))):
+            minutes += start_split[0][i]
         start_prefix = start_split[1]
         if start_prefix == "AM":
             digit_prefix = 1
         else:
             digit_prefix = 2
-        start_s_split = start_split[0].split(":")
-        hours = start_s_split[0]
-        minutes = start_s_split[1]
 
         return hours, minutes, digit_prefix
-        
-            
-#функция для проверки присваемого значения AM/PM   
-def reset_time(hours):
-    pass
-  
+
+def output(hours, minutes, prefix, days_later):
+    res = hours + ":" + minutes 
+    res += " "+"".join(prefix)
+    if days_later == 1:
+        res += " " + "".join("(next day)")
+    elif days_later > 1:
+        res += " " + "".join(f"({days_later} days later)")
+    return res
+
 #функция для сложения минут
 def calculate_minutes(s_min, d_min):
     res = int(s_min) + int(d_min)
+    new_hours = 0
     if res >= 60:
         new_min = res % 60 
-        new_hours = res // 60
-        return new_hours, new_min
-    return res
+        new_hours += res // 60
+        if new_min < 10:
+            new_min = "0" + str(new_min)
+        return new_hours, str(new_min)
+    return new_hours, str(res)
 
-def calculate_hours(s_hours, d_hours, n_hours = 0):
-    res = int(s_hours) + int(d_hours) + int(n_hours)
+def calculate_hours(s_hours, d_hours, dd_pref, n_hours=0):
+    # print(s_hours, d_hours, n_hours)
+    res = int(s_hours) + int(d_hours) + n_hours
     if res > 12:
+        pref_d = 0
         f_hours = res % 12
-        pref_d = res // 12
-        return f_hours, pref_d
-    return res
+        pref_d += res // 12
+        pref_d = pref_d + dd_pref
+        pref_d = pref_d // 2
+
+        if pref_d % 2 ==0:
+            f_pref = "PM"
+        else:
+            f_pref = "AM"
+        return f_pref, str(f_hours), pref_d
+    
+    if dd_pref % 2== 0:
+        pref_d = "PM"
+    else:
+        pref_d = "AM"
+
+    return pref_d, str(res)
 
 #основная функция
 def add_time(start, duration):
     
     start_hours, start_minutes, start_prefix = time_split(start, True)
+    print(start_hours, start_minutes, start_prefix)
     duration_hours, duration_minutes = time_split(duration)
-    print(start_minutes, duration_minutes)
+    print(duration_hours, duration_minutes)
     check_list = [(calculate_minutes(start_minutes, duration_minutes))]
-    print(check_list)
+    # print(check_list, "первый - доп. часы, второй - минуты")
+    add_hours = check_list[0][0]
+    final_minutes = check_list[0][1]
     
-    if len(check_list) > 1:
-        add_hours = check_list[0]
-        final_minutes = check_list[1]
-    else:
-        final_minutes = check_list[0]
+    check_list1 = [(calculate_hours(start_hours, duration_hours, start_prefix, add_hours))]
+    print(check_list1, "первый - перфикс, второй часы")
+    add_prefix_digit = check_list1[0][0]
+    final_hours = check_list1[0][1] #сколько раз делилось на 12
+    count_dpr = check_list1[0][2]
     
-    check_list1 = [(calculate_hours(start_hours, duration_hours))]
-    print(check_list1)
-    if len(check_list1) > 1:
-        final_hours = check_list1[0]
-        add_prefix_digit = check_list1[1]
-    else:
-        final_hours = check_list1[0]
-        
-    return final_hours, final_minutes
+    new_time = output(final_hours, final_minutes, add_prefix_digit, count_dpr)
+    
+    return new_time
 
-
-start_input = "3:10 PM"
-duration_input = "3:30"
-
-# start_input = input("Add start time: ")
-# duration_input = input("Write amount of hours: ")
+start_input = "11:55 AM"
+duration_input = "24:00"
 
 print(add_time(start_input, duration_input))
